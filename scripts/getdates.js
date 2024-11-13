631666bd04e97cf45931a74b1348fffd52893312
other-world <jamie@other-world.org>
2024-11-13 10:56:21 -0700

    index.html updates and basic javascript

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+//document = "getdates.js";
+const yr = new Date().getFullYear();
+const lastModified = document.lastModified;
+const author = "Jamie Cummings";
+const place = "Utah, USA";
+
+// \u00A9 is the unicode for the copywrite symbol
+document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
