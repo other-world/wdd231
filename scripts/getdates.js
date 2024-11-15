fd30f9eda6fbf081ff2bf4345ccf34e068910fa8
other-world <jamie@other-world.org>
2024-11-15 10:29:57 -0700

    some cleanup

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,5 +6,9 @@ const author = "Jamie Cummings";
 const place = "Utah, USA";
 
 // \u00A9 is the unicode for the copywrite symbol
-document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
-document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
+//document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+//document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
+
+// Trying the Javascript using hte querySelector instead of getElementById
+document.querySelector('#copywrite').innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+document.querySelector('#modified').innerHTML = `Last Modified ${lastModified}`;
