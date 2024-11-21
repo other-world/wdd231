7ab96bb42ed4c846d8887d868993bf6874d97057
other-world <jamie@other-world.org>
2024-11-21 11:54:34 -0700

    Some styling updates for the Chamber of Commerce site.

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,16 @@
+//console.log("Hello World");
+//document = "getdates.js";
+let yr = new Date().getFullYear();
+let lastModified = document.lastModified;
+const author = "Jamie Cummings";
+const place = "Utah, USA";
+
+// \u00A9 is the unicode for the copywrite symbol
+//document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+//document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
+
+// Trying the Javascript using hte querySelector instead of getElementById
+document.querySelector('#copywrite').innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+document.querySelector('#lastModified').innerHTML = `Last Modified ${lastModified}`;
+
+$("a[href*='" + location.pathname + "']").addClass("current");
