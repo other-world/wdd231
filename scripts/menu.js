233c580191c0b3fb925b9e12666df584639de6e4
other-world <jamie@other-world.org>
2024-11-22 10:32:13 -0700

    Issues with responsive views.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,9 @@
 const hamButton = document.querySelector('#menu');
-const mainNav = document.querySelector('.navigation');
+const mainNav = document.querySelector('#animatedMenu');
+//const mainNav = document.querySelector('.navigation');
 
 hamButton.addEventListener('click', () => {
 	mainNav.classList.toggle('show');
 	hamButton.classList.toggle('show');
+	toggleMenuButton();
 });
