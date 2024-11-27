0d2f8fb0eddd45f7ada30445ec5e03bdab5c7b85
other-world <jamie@other-world.org>
2024-11-26 22:33:06 -0700

    All but the last details on the footer

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,6 +1,6 @@
 //console.log("Hello World");
 //document = "getdates.js";
-let yr = new Date().getFullYear();
+let yr = new Date().toDateString();
 let lastModified = document.lastModified;
 const author = "Jamie Cummings";
 const place = "Utah, USA";
@@ -10,7 +10,7 @@ const place = "Utah, USA";
 //document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
 
 // Trying the Javascript using hte querySelector instead of getElementById
-document.querySelector('#copywrite').innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+document.querySelector('#copywrite').innerHTML = `\u00A9 Provo Chamber of Commerce`;
 document.querySelector('#lastModified').innerHTML = `Last Modified ${lastModified}`;
 
 //$("a[href*='" + location.pathname + "']").addClass("current");
