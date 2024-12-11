28ca79e7e8299dcde92fdbf6e4e059f6b55908d3
other-world <jamie@other-world.org>
2024-12-11 12:51:55 -0700

    Everything but the formating is working.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,51 +1,66 @@
-import {memberships} from '../data/membership.js';
-console.log(memberships);
+import { memberships } from '../data/membership.js';
+//console.log(memberships);
 
-const showHere = document.querySelector("#showHere");
-const myDialog = document.querySelector("#mydialog");
-const myTitle = document.querySelector("#mydialog h2");
-const myInfo = document.querySelector("#mydialog p");
-const myClose = document.querySelector("#mydialog button");
+const membershipLevel = document.querySelector("#membershipLevel");
+const myDialog = document.querySelector("#membershipDialog");
+console.log(myDialog);
+const myTitle = document.querySelector("#membershipDialog h2");
+const myInfo = document.querySelector("#membershipDialog p");
+const myClose = document.querySelector("#membershipDialog button");
+const benefitsList = document.createElement("ul");
 
 
 myClose.addEventListener("click", () => myDialog.close());
-displayItems(temples);
+displayMembershipLevels(memberships);
 
 
-function displayItems(data) {
+function displayMembershipLevels(data) {
     data.forEach(element => {
-        //console.log(element);
-        const templeImage = document.createElement("img");
-        templeImage.src = `${url}${element.path}`;
+        console.log(element);
+        const membershipBox = document.createElement("div");
+        membershipBox.setAttribute("id", `${element.type}Membership`);
+        membershipBox.setAttribute("class", "memberBoxes");
+        const membershipHeader = document.createElement("h3");
+        membershipHeader.innerHTML = `${element.type} Membership Level`;
+        membershipBox.appendChild(membershipHeader);
+
+        membershipLevel.appendChild(membershipBox);
+        /*templeImage.src = `${url}${element.path}`;
         templeImage.alt = element.name;
         templeImage.setAttribute("width", "400");
-        templeImage.setAttribute("height", "250");
+        templeImage.setAttribute("height", "250");*/
 
-        templeImage.addEventListener("click", () => showInfo(element));
+        membershipBox.addEventListener("click", () => showInfo(element));
 
-        showHere.appendChild(templeImage);        
     });
 }
 
-/*function showInfo(data){
-    //set up suffix for good grammar
-    let suffix = "";
-    switch(data.number){
-        case '1':
-            suffix = "st";
-            break;
-        case '2':
-            suffix = "nd";
-            break;
-        case "3":
-            suffix = "rd";
-            break;
-        default:
-            suffix = "th";
-            break;
+function showInfo(data) {
+    let costString = ""
+    let benefits = data.benefits;
+    benefitsList.innerHTML="";
+    if (data.type == "NP"){
+        costString = "Free membership for non-profit organizations";
+    }
+    else {
+        costString = `\$${data.cost} per year`;
     }
 
-    myTitle.innerHTML = `${data.name}`;
-    myInfo.innerHTML = `This is the ${data.number}${suffix} temple of The Church of Jesus Christ of Latter-day Saints. The ${data.name} was dedicated ${data.dedicated} by ${data.person}.`;
+
+    myTitle.innerHTML = `${data.type} Membership`;
+    myInfo.innerHTML = `
+    <p><b>Cost:</b> ${costString}</p>
+    <p><b>Benefits:</b></p>`;
+    benefits.forEach(element => { 
+        console.log(element);
+        const individualBenefit = document.createElement("li");
+        individualBenefit.innerHTML = element;
+        benefitsList.appendChild(individualBenefit);
+    });
+
+    myDialog.appendChild(benefitsList);
+
+
+
     myDialog.showModal();
-}*/
+}
---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,35 @@
+const currentURL = window.location.href;
+const showInfo = document.querySelector('#thankYou');
+const applicationDate = new Date();
+
+const everything = currentURL.split('?');
+const formData = everything[1].split('&');
+
+console.log(formData);
+
+
+showInfo.innerHTML = `
+<h2>Thank you for applying!</h2>
+<h3>Someone will contact you shortly.</h3>
+<p>First Name: ${show("fname")}</p>
+<p>Last Name: ${show("lname")}</p>
+<p>Position: ${show("orgTitle")}</p>
+<p>Phone: ${show("phone")}</p>
+<p>Email: <a href="mailto:${show("email")}">${show("email")}</a></p>
+<p>Business Name: ${show("bizName")}</p>
+<p>Membership Level: ${show("membership")}</p><br><br>
+<p>Submitted on</>
+<p>${applicationDate}</p>
+
+`
+
+
+function show(keyValuePair){
+    formData.forEach(element => {
+        if (element.startsWith(keyValuePair)){
+            result = element.split('=')[1].replace('%40', '@');
+            //result = element.split('=')[1].replace('%2B', '+');
+        }
+    });
+    return(result);
+}
