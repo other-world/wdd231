765d9381f3ad5fe36ea6d8e84a82f231450eedd2
other-world <jamie@other-world.org>
2024-12-17 22:35:32 -0700

    Uploading material from wdd131 to help me feel like I'm accompishing something.

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -1,9 +1,8 @@
 const yr = new Date().getFullYear();
-const lastModified = document.lastModified;
-console.log(document.getElementById("modified"));
+const lastModified = new Date(document.lastModified);
 const author = "Jamie Cummings";
 const place = "Utah, USA";
 
 // \u00A9 is the unicode for the copywrite symbol
-document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
-document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
+document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author}`;
+document.getElementById("modified").innerHTML = `Last Modified ${lastModified.toDateString()}`;
